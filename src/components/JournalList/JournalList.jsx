00d8f35b7eaa0ddd791fd2751a3cf343986eecc5
Dmitriy Morkulev <morkulev.dim@yandex.ru>
2024-04-20 19:22:21 +0300

    useMemo for memo filteredItems

--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,15 +1,11 @@
 import './JournalList.css';
 import CardButton from '../CardButton/CardButton';
 import JournalItem from '../JournalItem/JournalItem';
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import { UserContext } from '../../context/user.context';
 
 function JournalList({ items }) {
     const { userId } = useContext(UserContext);
-
-    if (items.length === 0) {
-        return <p>Записей пока нет, добавьте первую</p>;
-    }
     const sortItems = (a, b) => {
         if (a.date < b.date) {
             return 1;
@@ -17,21 +13,27 @@ function JournalList({ items }) {
             return -1;
         }
     };
+    // сейчас зависим только от userId и items, но если бы был, например, еще один частоизменяемый пропс, то каждый раз бы выполнялась перефильтрация и пересортировка
+    const filteredItems = useMemo(
+        () => items.filter((el) => el.userId === userId).sort(sortItems),
+        [items, userId]
+    ); // каждый раз при смене items или userId будем заново фильтровать и сортировать
+
+    if (items.length === 0) {
+        return <p>Записей пока нет, добавьте первую</p>;
+    }
 
     return (
         <>
-            {items
-                .filter((el) => el.userId === userId)
-                .sort(sortItems)
-                .map((el) => (
-                    <CardButton key={el.id}>
-                        <JournalItem
-                            title={el.title}
-                            text={el.text}
-                            date={el.date}
-                        />
-                    </CardButton>
-                ))}
+            {filteredItems.map((el) => (
+                <CardButton key={el.id}>
+                    <JournalItem
+                        title={el.title}
+                        text={el.text}
+                        date={el.date}
+                    />
+                </CardButton>
+            ))}
         </>
     );
 }
